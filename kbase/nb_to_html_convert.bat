7e92016b12cde7cf9bf9b165724d2dc3d7444940
doublebigmak <mak.michael.609@gmail.com>
2022-03-22 21:54:32 -0700

    kbase run

---
+++ kbase/nb_to_html_convert.bat
@@ -0,0 +1,6 @@
+jupyter nbconvert resales.ipynb --to html --no-input
+jupyter nbconvert index.ipynb --to html --no-input
+jupyter nbconvert economics.ipynb --to html --no-input
+jupyter nbconvert demographics.ipynb --to html --no-input
+jupyter nbconvert starts.ipynb --to html --no-input
+jupyter nbconvert rental.ipynb --to html --no-input
